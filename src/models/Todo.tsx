6368aaba59cbca2fd922231fc5c5d9549a684f00
Lau Jun Kit Darrick <darrekt@Darricks-MBP.home>
2020-11-28 11:05:11 +0000

    Implement immutable Todos

--- src/models/Todo.tsx
+++ src/models/Todo.tsx
@@ -1,8 +1,37 @@
-export default interface Todo {
+export default class Todo {
+  id: string;
+  name: string;
+  due: Date;
+  notes: string;
+  disableNotifications: boolean;
+  isRunning: boolean;
+
+  constructor(
     id: string,
     name: string,
     due: Date,
     notes: string,
     disableNotifications: boolean,
-    isRunning: boolean,
+    isRunning: boolean
+  ) {
+    this.id = id;
+    this.name = name;
+    this.due = due;
+    this.notes = notes;
+    this.disableNotifications = disableNotifications;
+    this.isRunning = isRunning;
+  }
+
+  fromFirestore = (doc: any) =>
+    new Todo(
+      doc.id,
+      doc.name,
+      doc.due,
+      doc.notes,
+      doc.disableNotifications,
+      doc.isRunning
+    );
+
+  // TODO
+  toFirestore() {}
 }
--- src/models/TodoRepository.tsx
+++ src/models/TodoRepository.tsx
@@ -1,1 +1,19 @@
+import { List } from "immutable";
 import Todo from "./Todo";
+
+export default class TodoRepository {
+  todos: List<Todo>;
+
+  constructor(todos: List<Todo> = List()) {
+    this.todos = todos;
+  }
+
+  addTodo = (curr: Todo) => (this.todos = this.todos.concat([curr]));
+  updateTodo = (curr: Todo) =>
+    (this.todos = this.todos.update(
+      this.todos.findIndex((item) => item.id == curr.id),
+      (item) => (item = curr)
+    ));
+  deleteTodo = (curr: Todo) =>
+    (this.todos = this.todos.filter((item) => item.id !== curr.id));
+}
