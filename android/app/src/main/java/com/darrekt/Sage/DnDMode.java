53ebf08b1565664d5ace98fb0b71b7d2e524a876
Darrekt <jkricklau@gmail.com>
2021-05-31 12:14:39 +0100

    DnD Mode WIP

--- android/app/src/main/java/com/darrekt/Sage/DnDMode.java
+++ android/app/src/main/java/com/darrekt/Sage/DnDMode.java
@@ -4,16 +4,39 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 
+import androidx.core.app.ActivityCompat;
+
+
 public class DnDMode extends ReactContextBaseJavaModule {
+    private NotificationManager notificationManager;
+    private boolean dndSupported;
     DnDMode(ReactApplicationContext context) {
         super(context);
+        notificationManager = (NotificationManager) context.getApplicationContext().getSystemService(context.NOTIFICATION_SERVICE);
+                dndSupported = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
     }
 
     @ReactMethod
     public void turnOnDND() {
-        Log.d("DnDMode", "DnD Mode turned on.");
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ){
+            if(!notificationManager.isNotificationPolicyAccessGranted()) {
+                getReactApplicationContext().startActivity(new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS));
+//                ActivityCompat.requestPermissions(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS, [Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS], 0);
+            }
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_PRIORITY);
+            Log.d("DnDMode", "DnD Mode turned on.");
+    }
+        else {
+            Log.d("DnDMode", "DnD Mode not supported.");
+        }
+
     }
 
     @ReactMethod
