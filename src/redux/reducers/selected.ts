a38a0fd61901cbf7fc6b6f4ea718400270a9547f
Darrekt <dl5215@ic.ac.uk>
2021-06-12 13:10:08 +0100

    Fix hydrating selection

--- src/redux/reducers/selected.ts
+++ src/redux/reducers/selected.ts
@@ -1,12 +1,18 @@
-import {
-  Action,
-  Actions,
-} from "../actions/actionTypes";
+import { Action, Actions } from "../actions/actionTypes";
 
 const selectedReducer = (state = "", action: Action) => {
-  if (action.type === Actions.TodoSelect) {
-    return state === action.payload.id ? "" : action.payload.id;
-  } else return state;
+  switch (action.type) {
+    case Actions.ProjectHydrate:
+      return (
+        action.payload
+          .find((project) => project.todos.some((todo) => todo.finishingTime))
+          ?.todos.find((todo) => todo.finishingTime)?.id ?? ""
+      );
+    case Actions.TodoSelect:
+      return state === action.payload.id ? "" : action.payload.id;
+    default:
+      return state;
+  }
 };
 
 export default selectedReducer;
