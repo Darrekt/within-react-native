9025e3510e5eda87e9746be057b30af87ba02f81
cy1719 <59970706+cy1719@users.noreply.github.com>
2022-06-13 14:09:30 +0100

    Finished front end of stats screen
    Changed the card size on EditSettingScreen to ensure that text fits the card when there is an error

--- src/screens/StatScreen.tsx
+++ src/screens/StatScreen.tsx
@@ -3,10 +3,11 @@ import { View, Text } from "react-native";
 import { globalStyles } from "../../styles";
 import DateListView from "../components/todo/DateListView";
 import ProjectProgressGraph from "../components/todo/ProjectProgressGraph";
+import ProjectProgressBarChart from "../components/todo/ProjectProgressBarChart";
 
 export const StatScreen = () => (
   <View style={globalStyles.centered}>
-    <ProjectProgressGraph></ProjectProgressGraph>
+    <ProjectProgressBarChart></ProjectProgressBarChart>
   </View>
 );
 
