35763931e2ac0c100134be634b4cefc703e06de9
cy1719 <59970706+cy1719@users.noreply.github.com>
2022-06-26 19:22:39 +0100

    Finished Stats Page
    Stats page can be used with calendar view to select the 7 day period to be shown on the graph.

--- src/components/todo/ProjectProgressBarChart.tsx
+++ src/components/todo/ProjectProgressBarChart.tsx
@@ -1,20 +1,37 @@
-import React from "react";
-import { Dimensions, useWindowDimensions } from "react-native";
+import React, { useState } from "react";
+import { Dimensions, useWindowDimensions, StyleSheet, Platform, Text } from "react-native";
 import { LineChart, BarChart } from "react-native-chart-kit";
 import HeadingDropDown from "../layout/HeadingDropDown";
 import { useAppSelector } from "../../redux/hooks";
 import { getTheme } from "../../redux/selectors";
-import { ScrollView } from "react-native-gesture-handler";
+import { Icon } from "react-native-elements";
 import { TodoEntity } from "../../models/Todo";
 import { getCompletedProjects, getCompletedTodos } from "../../redux/selectors";
 import { compareCompleted } from "../../util/compareDates";
 import TodoItemTile from "./TodoItemTile";
 import { Collection, List } from "immutable";
 import { compose } from "redux";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { globalStyles, textStyles } from "../../../styles";
+
+const styles = StyleSheet.create({
+  nameCol: {
+    ...globalStyles.column,
+    width: Dimensions.get("screen").width * 0.55,
+  },
+  dateCol: {
+    width: 0.3 * Dimensions.get("screen").width,
+    justifyContent: "center",
+  },
+  nameInput: { ...globalStyles.inputBox, width: "100%" },
+});
 
 export default function ProjectProgressGraph() {
+  var now = new Date();
   const windowDimensions = useWindowDimensions();
   const theme = useAppSelector(getTheme);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [endDay, setendDay] = useState(now.getTime());
   const iitems: TodoEntity[] = useAppSelector(getCompletedTodos);
   const groupItems = List(iitems as TodoEntity[])
     .sort(compareCompleted)
@@ -25,9 +42,8 @@ export default function ProjectProgressGraph() {
    const Moment = require('moment');
    const MomentRange = require('moment-range');
    const moment = MomentRange.extendMoment(Moment);
-   var now = new Date(); 
    var days = 6;
-   let start = (now.getTime()- days*24*60*60*1000), end = (now.getTime())
+   let start = (endDay - days*24*60*60*1000), end = endDay;
    let date = []
    date.push(moment(start).toDate().toDateString());
 while(moment(start) <= moment(end).subtract(1, 'days')){
@@ -37,14 +53,15 @@ while(moment(start) <= moment(end).subtract(1, 'days')){
 }
 var tododay ={};
 date.forEach((item) => {if(item){(tododay as any)[item] = 0}})
-groupItems.forEach((item) =>{ if(item[0] != null){(tododay as any)[item[0]] = item[1].count()}})
-
+console.log(tododay)
+groupItems.forEach((item) =>{ if(item[0] != null){if(item[0] in tododay){(tododay as any)[item[0]] = item[1].count()}}})
+console.log(date)
 
 
     return (
    <HeadingDropDown 
     header="Todos Finished Per Day"
-    topMargin={-50}
+    topMargin={20}
     headerStyle={{ marginLeft: windowDimensions.width * 0.22 }}
     >
       <BarChart
@@ -87,6 +104,30 @@ groupItems.forEach((item) =>{ if(item[0] != null){(tododay as any)[item[0]] = it
           borderRadius: 16,
         }}
       />
+      <Text>
+        Select a date to see the statistics of 7 days period!
+      </Text>
+      <Icon 
+      size= {60}
+      color = {theme.dark}
+      name = "event"
+      onPress ={() => setShowDatePicker(true)}
+       >
+      </Icon>
+      {showDatePicker && (
+      <DateTimePicker
+              style={styles.dateCol}
+              value={moment(now.getTime()).toDate()}
+              mode="date"
+              is24Hour={true}
+              display="default"
+              maximumDate={moment(now.getTime()).toDate()}
+              onChange={(event, selectedDate) => {
+                setShowDatePicker(Platform.OS === "windows");
+                setendDay(selectedDate);
+              }}>
+           </DateTimePicker>
+      )}
     </HeadingDropDown>
   );
 }
